2b5ffe6f3a0cf573519cd0dd3ad8ab6772f49c3d
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 18:37:06 -0300

    adicionado estilizaçao e formulario com validaçoes na pagina de cadastro

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,8 @@
 import Container from "../../components/container";
 import Input from "../../components/imput";
+import logoImg from "../../assets/logo.svg";
+
+import { Link } from "react-router-dom";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -20,8 +23,52 @@ export default function Register() {
   });
 
   return (
-    <div>
-      
-    </div>
+    <Container>
+			<div className="w-full justify-center items-center flex flex-col min-h-screen gap-4" >
+				<Link to='/' className="mb-6 max-w-sm w-full" >
+					<img
+					className="w-full"
+						src={ logoImg }
+						alt="Logo do site"
+					/>
+				</Link>
+
+				<form className="bg-white max-w-xl w-full rounded-lg p-4">
+					<div className="mb-3" >
+						<Input
+							type='text'
+							placeholder='Digite seu nome completo...'
+							name='name'
+							error={ errors.name?.message }
+							register={ register }
+						/>
+					</div>
+
+					<div className="mb-3" >
+						<Input
+							type='email'
+							placeholder='Digite seu email...'
+							name='email'
+							error={ errors.email?.message }
+							register={ register }
+						/>
+					</div>
+
+					<div className="mb-3" >
+						<Input
+							type='password'
+							placeholder='Digite sua senha...'
+							name='password'
+							error={ errors.password?.message }
+							register={ register }
+						/>
+					</div>
+
+					<button  type="submit" className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium" >Cadastrar</button>
+				</form>
+
+        <Link to='/login' >Já possui uma conta? Faça login</Link>
+			</div>
+		</Container>
   );
 };
