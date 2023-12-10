fbfcbce6d420fc4f68ee29809cfa35e7b077a2ef
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 13:03:06 -0300

    feat: criado arquivo das paginas, home, cardetail, dashboard e new

---
+++ src/pages/cardetail/index.tsx
@@ -0,0 +1,7 @@
+export default function CarDetail() {
+  return (
+    <div>
+      <h1>Page CarDetail</h1>
+    </div>
+  );
+};
---
+++ src/pages/dashboard/new/index.tsx
@@ -0,0 +1,7 @@
+export default function New() {
+  return (
+    <div>
+      <h1>Page New</h1>
+    </div>
+  );
+};
