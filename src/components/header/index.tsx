25a99ecb6894ae4bbb1dee28fa30a4cb2425e552
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 13:52:06 -0300

    feat: criado componente header que sera usado em mais de uma pagina do projeto

---
+++ src/components/header/index.tsx
@@ -0,0 +1,33 @@
+import logoImg from "../../assets/logo.svg";
+import { FiUser, FiLogIn } from "react-icons/fi";
+
+import { Link } from "react-router-dom";
+
+export default function Header(){
+  const signed = false;
+
+  return(
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4" >
+      <header className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto" >
+        <Link to='/' >
+          <img
+            src={ logoImg }
+            alt="Imagem da Logo"
+          />
+        </Link>
+        
+        <Link to={ signed ? '/dashboard' : '/login' } >
+          { signed ? (
+            <div className="border-2 rounded-full p-1 border-gray-900" >
+              <FiUser size={24} color="#000" />
+            </div>
+          ) : (
+            <div className="border-2 rounded-full p-1 border-gray-900" >
+              <FiLogIn size={24} color="#000" />
+            </div>
+          ) }
+        </Link>
+      </header>
+    </div>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,10 @@
+import Header from "../../components/header";
+
 export default function Home() {
   return (
     <div>
+      <Header/>
+
       <h1>Page Home</h1>
     </div>
   );
