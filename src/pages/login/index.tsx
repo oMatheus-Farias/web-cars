722991e438650e40c1a2ccb91b9f6d08b549c663
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 17:48:22 -0300

    feat: inserida imagem da logo do projeto na pagina de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,20 @@
+import logoImg from "../../assets/logo.svg";
+import Container from "../../components/container";
+
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Page Login</h1>
-    </div>
+    <Container>
+			<div className="w-full justify-center items-center flex flex-col min-h-screen gap-4" >
+				<Link to='/' className="mb-6 max-w-sm w-full" >
+					<img
+					className="w-full"
+						src={ logoImg }
+						alt="Logo do site"
+					/>
+				</Link>
+			</div>
+		</Container>
   );
 };
