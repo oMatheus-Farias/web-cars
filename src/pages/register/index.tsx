9038d1da1047cc25416fb1cc3de02db74c7ba7ba
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 18:32:38 -0300

    feat: adicinado configuraçao do schema na pagina de regsiter para validar o formulario de cadastro

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,7 +1,27 @@
+import Container from "../../components/container";
+import Input from "../../components/imput";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().nonempty('O campo nome é obrigatório'),
+  email: z.string().email('Digite um email válido').nonempty('O campo email é obrigatório'),
+  password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres').nonempty('O campo senha é obrigatório')
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function Register() {
+  const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange"
+  });
+
   return (
     <div>
-      <h1>Page Register</h1>
+      
     </div>
   );
 };
