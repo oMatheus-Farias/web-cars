edf13664ccce5fb3108b5706f9ae96bc770fd320
unknown <matheusfariasdasneves@gmail.com>
2023-12-10 20:22:37 -0300

    feat: adicionado configuraçao inicial do react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Toaster } from 'react-hot-toast'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Toaster
+      position='top-right'
+      reverseOrder={false}
+    />
     <App />
   </React.StrictMode>,
 )
