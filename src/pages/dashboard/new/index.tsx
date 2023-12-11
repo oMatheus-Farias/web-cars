dd986a5877c7ff88af84ef911c751c32b5091abd
unknown <matheusfariasdasneves@gmail.com>
2023-12-11 02:24:54 -0300

    feat: adicionado funcionalidade de enviar as imagens do carro para o banco de dados e a funcionalidade de enviar o formulario e salvar os dados no banco de dados

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,13 +1,21 @@
-import Header from "../../../components/header";
+import { ChangeEvent, useContext, useState } from "react";
 import Container from "../../../components/container";
 import PanelHeader from "../../../components/panelHeader";
-import Input from "../../../components/imput";
 
-import { FiUpload } from "react-icons/fi";
+import { FiUpload, FiTrash } from "react-icons/fi";
 
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from "../../../components/imput";
+
+import { AuthContext } from "../../../contexts";
+import { v4 as uuidV4 } from "uuid";
+import { storage, db } from "../../../services/firebaseConnection";
+import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
+
+import toast from "react-hot-toast";
 
 const schema = z.object({
   name: z.string().nonempty('O campo nome é obrigatório'),
@@ -17,27 +25,125 @@ const schema = z.object({
   price: z.string().nonempty('O valor do carro é obrigatório'),
   city: z.string().nonempty('A cidade é obrigatória'),
   whatsapp: z.string().min(1, 'O telefone é obrigatório').refine((value) => /^(\d{11,12})$/.test(value), {
-    message: 'Numero de telefone inválido'
+      message: 'Numero de telefone inválido'
   }),
   description: z.string().nonempty('A descrição é obrigatória')
 });
 
 type FormData = z.infer<typeof schema>;
 
-export default function New() {
-  const { register, reset, handleSubmit, formState: { errors } } = useForm<FormData>({
+interface ImageItemProps{
+  uid: string | number,
+  name: string,
+  previewUrl: string,
+  url: string
+};
+
+export default function New(){
+  const { user } = useContext(AuthContext);
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
     resolver: zodResolver(schema),
-    mode: "onChange"
+    mode: 'onChange'
   });
 
+  const [carImages, setCarImages] = useState<ImageItemProps[]>([]);
+
   function onSubmit(data: FormData){
-    console.log(data);
+    if(carImages.length === 0){
+      toast.error('Envie pelo menos uma imagem desse carro');
+      return;
   };
 
-  return (
-    <div>
-      <Header/>
+    const carListImages = carImages.map((car) => {
+      return {
+        uid: car.uid,
+        name: car.name,
+        url: car.url
+      };
+    });
 
+    addDoc(collection(db, 'cars'), {
+      name: data.name.toUpperCase(),
+      model: data.model,
+      year: data.year,
+      km: data.km,
+      price: data.price,
+      city: data.city,
+      whatsapp: data.whatsapp,
+      description: data.description,
+      created: new Date(),
+      owner: user?.name,
+      uid: user?.uid,
+      images: carListImages
+    })
+    .then(() => {
+      reset();
+      setCarImages([]);
+      toast.success('Cadastrado com sucesso');
+    })
+    .catch((error) => {
+      console.log('Erro ao enviar formulário', error);
+    });
+  };
+
+  async function handleFile(event: ChangeEvent<HTMLInputElement>){
+    if(event.target.files && event.target.files[0]){
+      const image = event.target.files[0];
+
+      if(image.type === 'image/jpeg' || image.type === 'image/png'){
+        await handleUpload(image);
+      }else{
+        alert('Envie uma imagem do tipo JPEG ou PNG');
+        return;
+      };
+    };
+  };
+
+  async function handleUpload(image: File){
+    if(!user?.uid){
+      return;
+    };
+
+    const currentUid = user?.uid;
+    const uidImage = uuidV4();
+
+    const uploadRef = ref(storage, `image/${currentUid}/${uidImage}`);
+
+    uploadBytes(uploadRef, image)
+    .then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((dowloadUrl) => {
+        const imageItem = {
+          uid: currentUid,
+          name: uidImage,
+          previewUrl: URL.createObjectURL(image),
+          url: dowloadUrl
+        };
+
+        setCarImages((images) => [...images, imageItem]);
+      })
+    })
+    .catch((error) => {
+      console.log('Erro ao salvar imagem', error);
+    });
+  };
+
+  async function handleDeleteImage(item: ImageItemProps){
+    const imagePath = `image/${item.uid}/${item.name}`;
+    const imageRef = ref(storage, imagePath);
+
+    await deleteObject(imageRef)
+    .then(() => {
+      setCarImages(carImages.filter((car) => {
+        return car.url !== item.url;
+      }));
+    })
+    .catch((error) => {
+      console.log('Erro ao deletar imagem', error);
+    });
+  };
+
+    return(
       <Container>
         <PanelHeader/>
 
@@ -51,9 +157,25 @@ export default function New() {
                 type="file" 
                 accept="image/*" 
                 className="opacity-0 cursor-pointer" 
+                onChange={ handleFile }
               />
             </div>
           </button>
+
+            { carImages.map((item) => {
+              return (
+                <div key={ item.name } className="w-full h-32 flex items-center justify-center relative" >
+                  <button className='absolute' onClick={() => handleDeleteImage(item)} >
+                    <FiTrash size={28} color='#FFF' />
+                  </button>
+                  <img
+                    src={ item.previewUrl }
+                    alt="Foto do carro"
+                    className="rounded-lg w-full h-32 object-cover"
+                  />
+                </div>
+              )
+            }) }
         </div>
 
         <div className="w-full bg-white p-3 rounded-lg flex fle-col sm:flex-row items-center gap-2 mt-2" >
@@ -95,16 +217,16 @@ export default function New() {
                   />
                 </div>
 
-                  <div className="w-full" >
-                    <p className="mb-2 font-medium" >KM rodados</p>
-                    <Input
-                      type='text'
-                      register={ register } 
-                      name='km'
-                      error={ errors.km?.message }
-                      placeholder="Ex: 120.000"
-                    />
-                  </div>
+                <div className="w-full" >
+                  <p className="mb-2 font-medium" >KM rodados</p>
+                  <Input
+                    type='text'
+                    register={ register } 
+                    name='km'
+                    error={ errors.km?.message }
+                    placeholder="Ex: 120.000"
+                  />
+                </div>
               </div>
 
               <div className="flex w-full mb-3 flex-row items-center gap-4" >
@@ -159,7 +281,6 @@ export default function New() {
               </button>
           </form>
         </div>
-      </Container>   
-    </div> 
-  );
+      </Container>
+    );
 };
