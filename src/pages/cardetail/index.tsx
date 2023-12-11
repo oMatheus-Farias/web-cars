93d82caad01ae99228acc052cdb559f4cd19e7f4
unknown <matheusfariasdasneves@gmail.com>
2023-12-11 16:24:22 -0300

    feat: adicionado componente header na pagina de detalhes do carro

--- src/pages/cardetail/index.tsx
+++ src/pages/cardetail/index.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+
 import Container from '../../components/container';
+import Header from '../../components/header';
+
 import { FaWhatsapp } from "react-icons/fa";
 
 import { getDoc, doc } from 'firebase/firestore';
@@ -91,68 +94,72 @@ export default function CarDetail(){
   }, []);
 
   return(
-    <Container>
-      { car && (
-        <Swiper
-          slidesPerView={ sliderPerView }
-          pagination={{ clickable: true }}
-          navigation
-        >
-          { car?.images.map((image) => (
-            <SwiperSlide key={ image.name } >
-              <img
-                src={ image.url }
-                className='w-full h-96 object-cover'
-              />
-            </SwiperSlide>
-          )) }
-        </Swiper>
-      ) }
+    <div>
+      <Header/>
 
-      { car && (
-        <main className='w-full bg-white rounded-lg p-6 my-4' >
-          <div className='flex flex-col sm:flex-row mb-4 items-center justify-between' >  
-            <h1 className='font-bold text-3xl text-black' >{ car?.name }</h1>
-            <h1 className='font-bold text-3xl text-black' >R$ { car?.price }</h1>
-          </div>
-          <p>{ car?.model }</p>
-
-          <div className='flex w-full gap-6 my-4' >
-            <div className='flex flex-col gap-4' >
-              <div>
-                <p>Cidade</p>
-                <strong>{ car?.city }</strong>
-              </div>
-              <div>
-                <p>Ano</p>
-                <strong>{ car?.year }</strong>
-              </div>
-            </div>
-
-            <div className='flex flex-col gap-4' >
-              <div>
-                <p>KM</p>
-                <strong>{ car?.km }</strong>
-              </div>
-            </div>
-          </div>
-
-          <strong>Descrição:</strong>
-          <p className='mb-4' >{ car?.description }</p>
-
-          <strong>Telefone / Whatsapp</strong>
-          <p className='mb-4' >{ car?.whatsapp }</p>
-
-          <a
-            href={ `https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá, vi esse ${car?.name} no site WebCarros e fiquei interessado` }
-            target='_blank'
-            className='bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium cursor-pointer'
+      <Container>
+        { car && (
+          <Swiper
+            slidesPerView={ sliderPerView }
+            pagination={{ clickable: true }}
+            navigation
           >
-            Conversar com vendedor
-            <FaWhatsapp size={26}  color='#FFF' />
-          </a>
-        </main>
-      ) }
-    </Container>
+            { car?.images.map((image) => (
+              <SwiperSlide key={ image.name } >
+                <img
+                  src={ image.url }
+                  className='w-full h-96 object-cover'
+                />
+              </SwiperSlide>
+            )) }
+          </Swiper>
+        ) }
+
+        { car && (
+          <main className='w-full bg-white rounded-lg p-6 my-4' >
+            <div className='flex flex-col sm:flex-row mb-4 items-center justify-between' >  
+              <h1 className='font-bold text-3xl text-black' >{ car?.name }</h1>
+              <h1 className='font-bold text-3xl text-black' >R$ { car?.price }</h1>
+            </div>
+            <p>{ car?.model }</p>
+
+            <div className='flex w-full gap-6 my-4' >
+              <div className='flex flex-col gap-4' >
+                <div>
+                  <p>Cidade</p>
+                  <strong>{ car?.city }</strong>
+                </div>
+                <div>
+                  <p>Ano</p>
+                  <strong>{ car?.year }</strong>
+                </div>
+              </div>
+
+              <div className='flex flex-col gap-4' >
+                <div>
+                  <p>KM</p>
+                  <strong>{ car?.km }</strong>
+                </div>
+              </div>
+            </div>
+
+            <strong>Descrição:</strong>
+            <p className='mb-4' >{ car?.description }</p>
+
+            <strong>Telefone / Whatsapp</strong>
+            <p className='mb-4' >{ car?.whatsapp }</p>
+
+            <a
+              href={ `https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá, vi esse ${car?.name} no site WebCarros e fiquei interessado` }
+              target='_blank'
+              className='bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium cursor-pointer'
+            >
+              Conversar com vendedor
+              <FaWhatsapp size={26}  color='#FFF' />
+            </a>
+          </main>
+        ) }
+      </Container>
+    </div>
   );
 };
